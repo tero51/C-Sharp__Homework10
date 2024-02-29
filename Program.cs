8f78870ee93f446b1c24aa3f119939c5d3bef21d
tero51 <akmalmagistr@gmail.com>
2024-02-29 03:46:29 +0300

    add first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Исходный массив
+        string[] inputArray = { "Hello", "2", "world", ":-)" };
+
+        // Вызов функции для фильтрации строк
+        string[] resultArray = FilterStrings(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArray)
+        {
+            Console.Write($"{str} ");
+        }
+    }
+}
