0629073c06cf8bdc2adc4769f3f6315901904e4d
tero51 <akmalmagistr@gmail.com>
2024-02-29 03:48:14 +0300

    add method

--- Program.cs
+++ Program.cs
@@ -15,4 +15,28 @@
             Console.Write($"{str} ");
         }
     }
+
+    static string[] FilterStrings(string[] array)
+    {
+        int resultCount = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                resultCount++;
+            }
+        }
+
+        string[] resultArray = new string[resultCount];
+        int index = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index++] = str;
+            }
+        }
+
+        return resultArray;
+    }
 }
